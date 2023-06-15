783690b0e8548d1b58d8fccc73346810be7e47f1
SphenOz <jlayokun@gmail.com>
2023-06-14 20:35:14 -0700

    adding files

---
+++ src/components/TopBanner.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+
+export default function Navbar() {
+    return(
+        <>
+        <div className='Bar'>
+            <h1>Hi</h1>
+        </div>
+        </>
+    )
+}
